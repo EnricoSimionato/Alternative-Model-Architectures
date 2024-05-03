92c1c94dfa947cfb3aad7da857cca7a844989f42
enricosimionato <enrico.simionato@mail.polimi.it>
2024-05-03 11:15:18 +0200

    refactoring of the packages

--- src/gbm/__init__.py
+++ src/gbm/__init__.py
@@ -18,11 +18,6 @@ __all__ = [
     "OpenAssistantGuanacoDataset",
 
     "ClassifierModelWrapper",
-
-    "convert_bytes_in_other_units",
-    "compute_model_memory_usage",
-
-    "count_parameters",
 ]
 
 from gbm.layers.global_dependent_layer import (
@@ -53,8 +48,3 @@ from gbm.utils.lightning_models import (
     ClassifierModelWrapper,
 )
 
-from gbm.utils.memory_usage_utils import convert_bytes_in_other_units, compute_model_memory_usage
-
-from gbm.utils.parameters_count import count_parameters
-
-from gbm.utils.storage_utils import store_model_and_info, load_model_and_info
--- src/gbm/utils/__init__.py
+++ src/gbm/utils/__init__.py
@@ -1,1 +1,14 @@
+__all__ = [
+    "convert_bytes_in_other_units",
+    "compute_model_memory_usage",
 
+    "count_parameters",
+    "store_model_and_info",
+    "load_model_and_info",
+]
+
+from gbm.utils.memory_usage_utils import convert_bytes_in_other_units, compute_model_memory_usage
+
+from gbm.utils.parameters_count import count_parameters
+
+from gbm.utils.storage_utils import store_model_and_info, load_model_and_info
