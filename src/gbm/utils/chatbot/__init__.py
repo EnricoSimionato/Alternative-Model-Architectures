4cbcb5019325e6e51a0b06f0a2142f9eba77ca9c
enricosimionato <enrico.simionato@mail.polimi.it>
2024-05-17 11:53:20 +0200

    Added load_original_model_for_causal_lm to package __init__

--- src/gbm/utils/chatbot/__init__.py
+++ src/gbm/utils/chatbot/__init__.py
@@ -2,6 +2,7 @@ __all__ = [
     "get_conversation_example_1",
     "get_conversation_example_2",
     "start_conversation_loop",
+    "load_original_model_for_causal_lm",
 
     "OpenAssistantGuanacoDataset",
     "OpenAssistantGuanacoDataModule",
@@ -14,6 +15,8 @@ from gbm.utils.chatbot.conversation_utils import (
     get_conversation_example_1,
     get_conversation_example_2,
     start_conversation_loop,
+
+    load_original_model_for_causal_lm
 )
 
 from gbm.utils.chatbot.pl_datasets import (
