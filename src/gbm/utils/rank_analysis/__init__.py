bef16ccaf24604579a26e047bfe50e1da158d141
enricosimionato <enrico.simionato@mail.polimi.it>
2024-05-18 16:56:54 +0200

    Rank analysis

--- src/gbm/utils/rank_analysis/__init__.py
+++ src/gbm/utils/rank_analysis/__init__.py
@@ -2,4 +2,4 @@ __all__ = [
     'start_layers_delta_rank_analysis',
 ]
 
-from .layers_delta_rank_analysis import start_layers_delta_rank_analysis
+from gbm.utils.rank_analysis.layers_delta_rank_analysis import start_layers_delta_rank_analysis
