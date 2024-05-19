f47dc8a1374601ab3d0f8f600a2246b39634e7d8
enricosimionato <enrico.simionato@mail.polimi.it>
2024-05-19 17:02:11 +0200

    changes to __init__ of rank analysis

--- src/gbm/utils/rank_analysis/__init__.py
+++ src/gbm/utils/rank_analysis/__init__.py
@@ -1,5 +1,11 @@
 __all__ = [
-    'start_layers_delta_rank_analysis',
+    "start_layers_rank_analysis",
+    "start_layers_delta_rank_analysis",
+    "start_layers_delta_average_rank_analysis",
 ]
 
-from gbm.utils.rank_analysis.layers_delta_rank_analysis import start_layers_delta_rank_analysis
+from gbm.utils.rank_analysis.layers_delta_rank_analysis import (
+    start_layers_rank_analysis,
+    start_layers_delta_rank_analysis,
+    start_layers_delta_average_rank_analysis,
+)
