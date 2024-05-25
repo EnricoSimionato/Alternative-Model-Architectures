44cd8a94a8b3295c6548ebcd696c278c1d745216
enricosimionato <enrico.simionato@mail.polimi.it>
2024-05-25 11:03:50 +0200

    function to check for nans in a model

--- src/gbm/utils/__init__.py
+++ src/gbm/utils/__init__.py
@@ -6,6 +6,8 @@ __all__ = [
     "convert_bytes_in_other_units",
     "compute_model_memory_usage",
 
+    "check_model_for_nan",
+
     "count_parameters",
 
     "store_model_and_info",
@@ -18,6 +20,8 @@ from gbm.utils.pipeline.experiment import Experiment
 from gbm.utils.time_usage_utils import convert_seconds_in_other_units
 
 from gbm.utils.memory_usage_utils import convert_bytes_in_other_units, compute_model_memory_usage
+
+from gbm.utils.models_utils import check_model_for_nan
 
 from gbm.utils.parameters_count import count_parameters
 
---
+++ src/gbm/utils/models_utils.py
@@ -0,0 +1,36 @@
+import torch
+
+
+def check_model_for_nan(
+        model: torch.nn.Module
+) -> bool:
+    """
+    Check if there are NaNs in the model parameters or gradients.
+
+    Args:
+        model (torch.nn.Module):
+            The model to check for NaNs.
+
+    Returns:
+        bool:
+            True if there are NaNs in the model parameters or gradients, False otherwise.
+    """
+
+    nan_in_params = False
+    nan_in_grads = False
+
+    for name, param in model.named_parameters():
+
+        if torch.isnan(param).any():
+            print(f'NaN found in parameter: {name}')
+            nan_in_params = True
+        if param.grad is not None and torch.isnan(param.grad).any():
+            print(f'NaN found in gradient: {name}')
+            nan_in_grads = True
+
+    if not nan_in_params:
+        print('No NaNs found in model parameters.')
+    if not nan_in_grads:
+        print('No NaNs found in model gradients.')
+
+    return nan_in_params or nan_in_grads
