44f9cd6c0ddd5f1e439aeafc3b94806e9aa56bfa
enricosimionato <enrico.simionato@mail.polimi.it>
2024-05-28 12:39:09 +0200

    Returning all paths to logs

--- src/tests/gbm/utils/pipeline/test_experiment.py
+++ src/tests/gbm/utils/pipeline/test_experiment.py
@@ -5,21 +5,28 @@ from gbm.utils.pipeline.experiment import Experiment
 from gbm.utils.pipeline.config import Config
 from gbm.utils.chatbot.conversation_utils import load_original_model_for_causal_lm
 from gbm.utils.chatbot import OpenAssistantGuanacoDataModule
+from gbm.utils.chatbot.conversation_utils import start_conversation_loop
 
+configuration = Config(
+    "/Users/enricosimionato/Desktop/Alternative-Model-Architectures/src/experiments/configurations/CONFIG_BERT_CLASS.json"
+)
+original_model = load_original_model_for_causal_lm(configuration)
+tokenizer = AutoTokenizer.from_pretrained(configuration.get("tokenizer_id"))
 
 def test_experiment_with_chatbot():
     configuration = Config(
-        "/Users/enricosimionato/Desktop/Alternative-Model-Architectures/src/experiments/configurations/CONFIG_BERT_CHAT.json"
+        "/Users/enricosimionato/Desktop/Alternative-Model-Architectures/src/experiments/configurations/CONFIG_GEMMA_CHAT.json"
     )
     original_model = load_original_model_for_causal_lm(configuration)
     tokenizer = AutoTokenizer.from_pretrained(configuration.get("tokenizer_id"))
-    tokenizer.bos_token = "[CLS]"
-    tokenizer.eos_token = "[SEP]"
+    #tokenizer.bos_token = "[CLS]"
+    #tokenizer.eos_token = "[SEP]"
     tokenizer.padding_side = "right"
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.chat_template = AutoTokenizer.from_pretrained(
         configuration.get("tokenizer_id_for_chat_template")).chat_template
 
+    """
     # Create the global model
     global_model = GlobalBaseModel(
         original_model,
@@ -35,10 +42,11 @@ def test_experiment_with_chatbot():
         preserve_original_model=True,
         verbose=True
     )
+    """
 
     experiment = Experiment(
         task="chatbot",
-        model=global_model,
+        model=original_model,
         dataset=OpenAssistantGuanacoDataModule(
             configuration.get("batch_size"),
             configuration.get("num_workers"),
@@ -94,10 +102,42 @@ def test_experiment_with_classifier():
         config=configuration,
         tokenizer=tokenizer
     )
-    experiment.display_tensorboard_logs()
 
     experiment.run_experiment()
 
 
+def test_storage_utilities_of_experiment():
+    experiment = Experiment(
+        task="chatbot",
+        model=original_model,
+        dataset=OpenAssistantGuanacoDataModule(
+            configuration.get("batch_size"),
+            configuration.get("num_workers"),
+            tokenizer,
+            configuration.get("max_len_tokenizer"),
+            configuration.get("split")
+        ),
+        config=configuration,
+        tokenizer=tokenizer
+    )
+    experiment.start_experiment()
+
+    experiment.store_experiment()
+
+    Experiment.load_experiment(configuration.get("path_to_experiment"))
+
+
+    start_conversation_loop(
+        experiment.get_model(),
+        experiment.get_tokenizer(),
+        experiment.get_config().get("stop_tokens"),
+        user_inputs=["Hello", "How are you?"]
+    )
+
+
 if __name__ == "__main__":
-    test_experiment_with_chatbot()
+    sentence = "I want"
+    print(sentence)
+    print(tokenizer.decode(original_model.generate(tokenizer.encode(sentence, return_tensors="pt")).tolist()[0], skip_special_tokens=True))
+
+    test_storage_utilities_of_experiment()
