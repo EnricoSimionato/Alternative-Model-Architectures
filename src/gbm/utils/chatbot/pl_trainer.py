7b5c0112b14827a9d20417ebe5b83b652c579a3b
enricosimionato <enrico.simionato@mail.polimi.it>
2024-06-19 17:47:49 +0200

    KFC changes

--- src/gbm/utils/chatbot/pl_trainer.py
+++ src/gbm/utils/chatbot/pl_trainer.py
@@ -29,12 +29,12 @@ def get_causal_lm_trainer(
     # Defining callbacks
     callbacks = [
         # Defining early stopping callback
-        pl.callbacks.EarlyStopping(
-            monitor="validation_loss",
-            min_delta=0.001,
-            patience=3,
-            verbose=True
-        ),
+        #pl.callbacks.EarlyStopping(
+        #    monitor="validation_loss",
+        #    min_delta=0.001,
+        #    patience=3,
+        #    verbose=True
+        #),
         # Defining checkpointing callback
         pl.callbacks.ModelCheckpoint(
             dirpath=config.get("path_to_checkpoints"),
