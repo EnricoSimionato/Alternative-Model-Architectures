d8c4abd78ea76e208d4ddef20047e0c88f2d8bb9
enricosimionato <enrico.simionato@mail.polimi.it>
2024-07-22 16:25:10 +0200

    Configs

--- src/gbm/utils/experiment_pipeline/config_printer.py
+++ src/gbm/utils/experiment_pipeline/config_printer.py
@@ -8,7 +8,7 @@ def load_experiment_config(path_to_experiment):
     if not os.path.exists(config_path):
         raise Exception(f"Configuration file not found at: '{config_path}'")
 
-    with open(config_path, 'r') as f:
+    with open(config_path, "r") as f:
         config = json.load(f)
 
     return config
@@ -20,7 +20,7 @@ def main():
         sys.exit(1)
 
     experiment_name = sys.argv[1]
-    path_to_experiment = os.path.join(os.getcwd().split("src")[0], "src", "experiments", "performed_experiments", experiment_name)
+    path_to_experiment = os.path.join("src", "experiments", "performed_experiments", experiment_name)
 
     if not os.path.exists(path_to_experiment):
         print(f"Experiment '{experiment_name}' not found.")
