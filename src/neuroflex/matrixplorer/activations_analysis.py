994eea94030f02cea41168fcd1144353545f87e0
enricosimionato <enrico.simionato@mail.polimi.it>
2024-08-29 12:37:32 +0200

    configs and stuff

--- src/neuroflex/matrixplorer/activations_analysis.py
+++ src/neuroflex/matrixplorer/activations_analysis.py
@@ -3,7 +3,6 @@ import pickle as pkl
 from neuroflex.utils.classification import load_original_model_for_sequence_classification, IMDBDataModule, \
     load_tokenizer_for_sequence_classification
 
-from neuroflex.utils.printing_utils.printing_utils import Verbose
 from neuroflex.utils.experiment_pipeline import Config
 
 from neuroflex.utils.rank_analysis.utils import extract_based_on_path, AnalysisModelWrapper
@@ -11,7 +10,6 @@ from neuroflex.utils.rank_analysis.utils import extract_based_on_path, AnalysisM
 
 def perform_activations_analysis(
         configuration: Config,
-        verbose: Verbose
 ) -> None:
     """
     Performs the activations' analysis.
@@ -19,8 +17,6 @@ def perform_activations_analysis(
     Args:
         configuration (Config):
             The configuration object containing the necessary information to perform the analysis.
-        verbose (Verbose):
-            The verbosity level of the analysis.
     """
 
     # Getting the parameters related to the paths from the configuration
@@ -32,6 +28,12 @@ def perform_activations_analysis(
     # Getting the parameters related to the analysis from the configuration
     fig_size = configuration.get("figure_size") if configuration.contains("figure_size") else (20, 20)
 
+    # Getting the parameters related to the model and the data from the configuration
+    batch_size = configuration.get("batch_size") if configuration.contains("batch_size") else 64
+    num_workers = configuration.get("num_workers") if configuration.contains("num_workers") else 1
+    seed = configuration.get("seed") if configuration.contains("seed") else 42
+    max_len = configuration.get("max_len") if configuration.contains("max_len") else 512
+
     # Load the data if the file is available, otherwise process the model
     if file_available:
         print(f"The file '{file_path}' is available.")
@@ -41,13 +43,19 @@ def perform_activations_analysis(
         # Loading and wrapping the model
         model = load_original_model_for_sequence_classification(configuration)
         tokenizer = load_tokenizer_for_sequence_classification(configuration)
-        model_wrapper = AnalysisModelWrapper(model, verbose=verbose)
+        model_wrapper = AnalysisModelWrapper(model)
 
         # Loading the dataset
         dataset = IMDBDataModule(
-            tokenizer=model_wrapper.tokenizer,
-            max_len=model_wrapper.max_len,
-            batch_size=model_wrapper.batch_size,
-            num_workers=model_wrapper.num_workers,
-            seed=model_wrapper.seed
+            tokenizer=tokenizer,
+            max_len=max_len,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            split=(0.8, 0.1, 0.1),
+            seed=seed
         )
+
+        # Performing the activation analysis
+        data_loader = dataset.train_dataloader()
+        for batch in data_loader:
+            y = model_wrapper.forward(batch)
