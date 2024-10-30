7c8e4b564d8bde56396440956e341d612289ee44
enricosimionato <enrico.simionato@mail.polimi.it>
2024-10-30 18:28:41 +0100

    Debugging

--- docker_container.sh
+++ docker_container.sh
@@ -75,14 +75,14 @@ if [ -n "$EXISTING_CONTAINER" ]; then
         echo "Container $CONTAINER_NAME exists but is not running. Removing it."
         docker rm $EXISTING_CONTAINER
         echo "Creating and running a new container."
-        docker run -it --name $CONTAINER_NAME \
+        docker run -it --user $(id -u enricosimionato):$(id -g enricosimionato) --name $CONTAINER_NAME \
           -v $PWD/src/experiments:/Alternative-Model-Architectures/src/experiments \
           -v $HOME/.cache/huggingface:/root/.cache/huggingface \
           --gpus all $IMAGE_NAME || { echo "Failed to run container with image $IMAGE_NAME"; exit 1; }
     fi
 else
     echo "No existing container found. Creating and running a new one."
-    docker run -it --name $CONTAINER_NAME \
+    docker run -it --user $(id -u enricosimionato):$(id -g enricosimionato) --name $CONTAINER_NAME \
       -v $PWD/src/experiments:/Alternative-Model-Architectures/src/experiments \
       -v $HOME/.cache/huggingface:/root/.cache/huggingface \
       --gpus all -m 32g $IMAGE_NAME || { echo "Failed to run container with image $IMAGE_NAME"; exit 1; }
