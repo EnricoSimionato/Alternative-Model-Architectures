98566bb2bc4123499a0fd5efe2a94fdcedb64011
enricosimionato <enrico.simionato@mail.polimi.it>
2024-10-31 12:04:15 +0100

    Debugging

--- src/neuroflex/experiments/fine_tuning_experiment.py
+++ src/neuroflex/experiments/fine_tuning_experiment.py
@@ -47,6 +47,7 @@ class FineTuningExperiment(BenchmarkEvaluation):
             self.log(f"Error loading the model: {e}")
             raise e
         print(loaded_model)
+        
         self.log("Running the experiment ahfbasdibfaisdhbfkadbgfahbdf.", print_message=True)
 
 
