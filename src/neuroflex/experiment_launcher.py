119c28e0a01bfa026dc35df07602824138eec3a6
enricosimionato <enrico.simionato@mail.polimi.it>
2025-02-25 16:52:36 +0100

    Approximation error for the experiments I did in the past

--- src/neuroflex/experiment_launcher.py
+++ src/neuroflex/experiment_launcher.py
@@ -24,6 +24,27 @@ GeneralPurposeExperimentFactory.register({
 })
 
 
+#def main() -> None:
+"""
+    Main method to start the various types of experiments on a deep model.
+"""
+"""
+    if len(sys.argv) < 2:
+        raise Exception("Please provide the name of the configuration file.\n"
+                        "Example: python src/redhunter/analysis_launcher.py config_name")
+
+    # Extracting the configuration name and the environment
+    config_file_name = sys.argv[1]
+
+    # Creating and launching the experiment
+    experiment = GeneralPurposeExperimentFactory.create(f"src/experiments/configurations/{config_file_name}")
+    experiment.launch_experiment()
+    """
+
+import torch
+from exporch.utils.causal_language_modeling import load_model_for_causal_lm
+from exporch import Config
+
 def main() -> None:
     """
     Main method to start the various types of experiments on a deep model.
@@ -36,10 +57,47 @@ def main() -> None:
     # Extracting the configuration name and the environment
     config_file_name = sys.argv[1]
 
-    # Creating and launching the experiment
-    experiment = GeneralPurposeExperimentFactory.create(f"src/experiments/configurations/{config_file_name}")
-    experiment.launch_experiment()
+    num_layers = 12
+    #path = "src/experiments/results/bert-base-uncased/factorization_fine_tuning_experiment/version_0/GlobalBase.pt"
 
+    path = "src/experiments/results/Llama-3.1-8B/factorization_fine_tuning_experiment/version_92/LocalSVD.pt"
+    #path = "src/experiments/results/Llama-3.1-8B/factorization_fine_tuning_experiment/version_10/GlobalBase.pt"
+    #path = "src/experiments/results/Llama-3.1-8B/factorization_fine_tuning_experiment/version_14/GlobalBase.pt"
+
+    #path = "src/experiments/results/Llama-3.1-8B/factorization_fine_tuning_experiment/version_91/LocalSVD.pt"
+    #path = "src/experiments/results/Llama-3.1-8B/factorization_fine_tuning_experiment/version_11/GlobalBase.pt"
+    #path = "src/experiments/results/Llama-3.1-8B/factorization_fine_tuning_experiment/version_15/GlobalBase.pt"
+
+    config = Config(path.replace("GlobalBase.pt", "config.yaml").replace("LocalSVD.pt", "config.yaml"))
+    config.set("device", "cpu")
+
+    original_model = load_model_for_causal_lm(config)
+
+    with open(path, "rb") as f:
+        model = torch.load(f, weights_only=False)
+
+    avg_sse = 0
+    avg_rsse = 0
+
+    for i in range(num_layers):
+        #original_weight = original_model.bert.encoder.layer[i].attention.self.query.weight
+        original_weight = original_model.model.layers[i].mlp.gate_proj.weight
+        #original_weight = original_model.model.layers[i].mlp.up_proj.weight
+        #approximated_weight = model.bert.encoder.layer[i].attention.self.query.weight
+        approximated_weight = model.model.layers[i].mlp.gate_proj.weight
+        #approximated_weight = model.model.layers[i].mlp.up_proj.weight
+
+        sse = torch.sum((original_weight.to(torch.float32) - approximated_weight.to(torch.float32)) ** 2)
+        rsse = torch.sum((original_weight.to(torch.float32) - approximated_weight.to(torch.float32)) ** 2) / torch.sum(original_weight.to(torch.float32) ** 2)
+
+        avg_sse += sse
+        avg_rsse += rsse
+
+    avg_sse /= num_layers
+    avg_rsse /= num_layers
+
+    print(f"Average SSE: {avg_sse}")
+    print(f"Average RSSE: {avg_rsse}")
 
 if __name__ == "__main__":
     main()
